e086841682f3ffc3a472a80de0ff289da84f2f34
max <maxanjo512@gmail.com>
2017-08-19 00:18:32 +0400

    init

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var gulp         = require('gulp'),
 		gulpif = require('gulp-if'),
 		uglify = require('gulp-uglify'),
 		minifyCss = require('gulp-minify-css'),
+		sftp = require('gulp-sftp');
 		notify = require('gulp-notify');
 
 gulp.task('browser-sync', ['styles'], function() {
@@ -20,44 +21,54 @@ gulp.task('browser-sync', ['styles'], function() {
 		});
 });
 
+gulp.task('sftp', function () {
+    return gulp.src('./wordpress/wp-content/themes/project/*')
+        .pipe(sftp({
+            host: 'localhost',
+            user: 'a0130638_darius',
+            pass: '09081993',
+            remotePath: '/home/a0130638/domains/webove.ru/public_html/test/'
+        }));
+});
+
 
 gulp.task('styles', function () {
-	return gulp.src('./wordpress/wp-content/themes/webove/sass/*.sass')
+	return gulp.src('./wordpress/wp-content/themes/project/sass/*.sass')
 	.pipe(sass({
 		includePaths: require('node-bourbon').includePaths
 	}).on('error', sass.logError))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
 	.pipe(cleanCSS())
-	.pipe(gulp.dest('wordpress/wp-content/themes/webove/css'))
+	.pipe(gulp.dest('wordpress/wp-content/themes/project/css'))
 	.pipe(browserSync.stream());
 });
 
 // Sprites JPG PNG
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('./wordpress/wp-content/themes/webove/img/icons/*.png').pipe(spritesmith({
+  var spriteData = gulp.src('./wordpress/wp-content/themes/project/img/icons/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
     cssName: 'sprite.sass',
     padding: 10
   }));
-  return spriteData.pipe(gulp.dest('./wordpress/wp-content/themes/webove/css'));
+  return spriteData.pipe(gulp.dest('./wordpress/wp-content/themes/project/css'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./wordpress/wp-content/themes/webove/sass/*.sass', ['styles']);
-	gulp.watch('./wordpress/wp-content/themes/webove/scripts/*.js').on("change", browserSync.reload);
-	gulp.watch('./wordpress/wp-content/themes/webove/*.html').on('change', browserSync.reload);
-	gulp.watch('./wordpress/wp-content/themes/webove/**/*.php').on('change', browserSync.reload);
+	gulp.watch('./wordpress/wp-content/themes/project/sass/*.sass', ['styles']);
+	gulp.watch('./wordpress/wp-content/themes/project/scripts/*.js').on("change", browserSync.reload);
+	gulp.watch('./wordpress/wp-content/themes/project/*.html').on('change', browserSync.reload);
+	gulp.watch('./wordpress/wp-content/themes/project/**/*.php').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['jade', 'browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
 
 // Builder
 gulp.task('build', function () {
-    return gulp.src('*.html')
+    return gulp.src('./wordpress/wp-content/themes/project/*.php')
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(useref())
-        .pipe(gulp.dest('./wordpress/wp-content/themes/webove/'));
+        .pipe(gulp.dest('./wordpress/wp-content/themes/project/'));
 })
