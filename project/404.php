e613d286f7fe33f47faa355e918bc543c2fb6759
Max <maxanjo512@gmail.com>
2017-08-20 11:40:24 +0400

    Add files via upload

---
+++ project/404.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+<div class="container text-center not-found">
+	<h2>404</h2>
+	<p>Sorry, but the page you're looking for has not found. Try checking the URL for errors, then hit the refresh<br /> button on your browser</p>
+</div>
+<?php get_footer() ?>
