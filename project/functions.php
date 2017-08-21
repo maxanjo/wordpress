088e332d815d4cd9a96956fe71a5f7c28488fc39
Max <maxanjo512@gmail.com>
2017-08-21 21:25:05 +0400

    Update functions.php

--- project/functions.php
+++ project/functions.php
@@ -6,3 +6,6 @@ remove_action('wp_head', 'wlwmanifest_link');
 show_admin_bar(false);
 
 add_theme_support('post-thumbnails');
+
+// Load up our awesome theme options
+require_once ( get_stylesheet_directory() . '/theme-options.php' );
