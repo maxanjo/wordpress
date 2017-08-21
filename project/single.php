f5b9db1541755c513e869771f7200fd233f45270
Max <maxanjo512@gmail.com>
2017-08-21 18:15:51 +0400

    Update single.php

--- project/single.php
+++ project/single.php
@@ -1,61 +1,29 @@
 <?php get_header(); ?>
-    <!-- Content -->
-    <div class="blog-center-align">
+<!-- Content -->
+<div class="row single-page">
+    <div class="container">
+        <main class="col-sm-8">
 
-    <!-- Blog Caption -->
-    <div class="blog-single-caption">
-        <a class="backblog" href="<?php echo home_url(); ?>/blog"><?php _e('&lt; Back to the Blog','aletheme'); ?></a>
-    </div>
+            <!-- Blog Caption -->
+            <div class="blog-single-caption">
+                <a class="backblog" href="<?php echo home_url(); ?>/blog"><?php _e('&lt; Back to the Blog','aletheme'); ?></a>
+            </div>
 
-    <!-- Blog Line -->
-    <div class="blog-line"></div>
-
-    <!-- Blog Content -->
-    <div class="blog-single">
-
-        <!-- Right Menu -->
-        <div class="right-side">
-            <?php get_sidebar(); ?>
-        </div>
-
-        <!-- Left Content -->
-        <div class="left-side">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-                <?php
-                    echo get_the_post_thumbnail($post->ID,'post-minibox');
-                ?>
-
-
-
-
-
-                <p class="info"><?php _e('in','aletheme'); ?> <?php the_category(', '); ?> <?php _e('by','aletheme'); ?> <?php echo the_author_posts_link(); ?>  <span>|</span> <?php comments_number( 'no comments', 'one comment', '% comments' ); ?></p>
-                <h3 class="caption"><?php the_title(); ?></h3>
-                <div class="text">
-                    <?php ale_part('posthead');?>
-                    <?php ale_part('postfull');?>
-                    <div class="cf"></div>
-                    <?php ale_part('postfooter');?>
-                </div>
+            <!-- Article -->
+            <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                <article class="media">
+                    <?php the_content() ?>
+                </article>
             <?php endwhile; else: ?>
-                <?php ale_part('notfound')?>
             <?php endif; ?>
-            <!-- Line -->
-            <div class="blog-line"></div>
             <?php comments_template(); ?>
-        </div>
+        </main>
+        
+        <!-- Sidebar -->
+        <aside class="col-sm-4">
+            <?php get_sidebar() ?>
+        </aside>
     </div>
+</div>
 
-    <!-- Blog Line -->
-    <div class="blog-line"></div>
-
-    <!-- Blog Nav  -->
-    <div class="blog-single-footer">
-        <div class="left"><?php if(get_previous_post_link()){ ?><span class="left"></span><?php } ?> <?php previous_post_link('%link'); ?> </div>
-        <div class="right"><?php if(get_next_post_link()){ ?><span class="right"></span><?php } ?> <?php next_post_link('%link'); ?>  </div>
-    </div>
-
-    </div>
 <?php get_footer(); ?>
