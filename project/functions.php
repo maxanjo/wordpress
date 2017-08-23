2dc2c73c40cabb43b582d83db5292258eaf3e71c
Max <maxanjo512@gmail.com>
2017-08-23 21:30:10 +0400

    Update functions.php

--- project/functions.php
+++ project/functions.php
@@ -7,6 +7,10 @@ show_admin_bar(false);
 
 add_theme_support('post-thumbnails');
 
+register_nav_menu('primary', 'Primary menu');
+
 // Load up our awesome theme options
 require  get_template_directory() . '/inc/function-admin.php';
 require  get_template_directory() . '/inc/enqueue.php';
+
+
