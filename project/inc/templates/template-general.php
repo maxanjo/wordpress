34cc520c898eaf9d20809a0b8b0fcb5970658f15
Max <maxanjo512@gmail.com>
2017-08-23 20:01:04 +0400

    Update template-general.php

--- project/inc/templates/template-general.php
+++ project/inc/templates/template-general.php
@@ -1,2 +1,9 @@
-<h3 class="title">Manage Options</h3>
-<p>Customize the default Theme Appearance Options</p>
+<form method="post" action="options.php" class="web-form-general-form">
+<?php settings_errors(); ?> 
+
+	<!-- settings_fields(name of setting you registered(input))-->
+	<?php settings_fields('options_settings'); ?>
+	<!-- do_settings_section(calling settings you created on line 44) -->
+	<?php do_settings_sections('my_theme'); ?>
+	<?php submit_button('Save Changes', 'primary', 'btnSubmit'); ?>
+</form>
