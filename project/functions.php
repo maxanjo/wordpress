618818c101ab18edc68e0f3b3eedf05cc8fc0402
Max <maxanjo512@gmail.com>
2017-08-23 20:02:25 +0400

    Update functions.php

--- project/functions.php
+++ project/functions.php
@@ -9,3 +9,4 @@ add_theme_support('post-thumbnails');
 
 // Load up our awesome theme options
 require  get_template_directory() . '/inc/function-admin.php';
+require  get_template_directory() . '/inc/enqueue.php';
