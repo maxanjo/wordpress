841f50119388569f8a5a27d9844077d26404dd20
Max <maxanjo512@gmail.com>
2017-09-20 17:18:11 +0400

    Update function-admin.php

--- project/inc/function-admin.php
+++ project/inc/function-admin.php
@@ -17,7 +17,7 @@ function web_add_admin_page(){
  	add_submenu_page( 'my_theme', 'Webove Theme Options', 'General', 'manage_options', 'my_theme', 'Webove_theme_create_page' );
 	add_submenu_page( 'my_theme', 'Webove Social', 'Social', 'manage_options', 'my_theme_social', 'Webove_theme_setting_page' );
 	add_submenu_page( 'my_theme', 'Custom Css', 'Custom Css', 'manage_options', 'my_theme_css', 'Webove_custom_css_page' );
-	add_submenu_page( 'my_theme', 'Additional', 'Additional', 'manage_options', 'my_theme_additional', 'Webove_custom_additional_page' );
+	//add_submenu_page( 'my_theme', 'Additional', 'Additional', 'manage_options', 'my_theme_additional', 'Webove_custom_additional_page' );
 
 	//Activate Custom_Settings
 	//add_action(initialization, function name)
