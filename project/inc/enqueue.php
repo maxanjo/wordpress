d36f81fcff6c192c1f0dff77b62a79f36aaf97a9
Max <maxanjo512@gmail.com>
2017-09-20 16:46:10 +0400

    Update enqueue.php

--- project/inc/enqueue.php
+++ project/inc/enqueue.php
@@ -13,11 +13,12 @@ function web_load_admin_scripts($current_){
 		
 		wp_register_style('web-admin', get_template_directory_uri() . '/inc/css/template.admin.css', array(), '1.0.0', 'all');
 		wp_enqueue_style('web-admin');
-
-		wp_register_script('web-admin-script', get_template_directory_uri() . '/inc/js/template.admin.js', array('jquery'), '1.0.0', true);
-		wp_enqueue_script('web-admin-script');
 		wp_register_script('web-jquery', get_template_directory_uri() . '/inc/js/jquery.min.js', '1.0.0', true);
 		wp_enqueue_script('web-jquery');
+		wp_register_script('web-editor', 'https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js', '4.7.3', true);
+		wp_enqueue_script('web-editor');
+		wp_register_script('web-admin-script', get_template_directory_uri() . '/inc/js/template.admin.js', array('jquery'), '1.0.0', true);
+		wp_enqueue_script('web-admin-script');
 		wp_localize_script('web-jquery', 'directory', array( 'url' => get_template_directory_uri() ));
 		}else if('webove_page_my_theme_social' == $current_){		
 		wp_register_style('web-admin', get_template_directory_uri() . '/inc/css/template.admin.css', array(), '1.0.0', 'all');
