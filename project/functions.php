e04359ee00945162a04cc53c4a0ee99cc481217e
Max <maxanjo512@gmail.com>
2017-09-21 16:29:47 +0400

    Update functions.php

--- project/functions.php
+++ project/functions.php
@@ -17,3 +17,33 @@ function my_theme_setup(){
     load_theme_textdomain('webove', get_template_directory() . '/languages');
 }
 
+// SideBar
+function true_register_wp_sidebars() {
+ 
+    /* В боковой колонке - первый сайдбар */
+   /* register_sidebar(
+        array(
+            'id' => 'homepage_sidebar', 
+            'name' => 'Home page sidebar', 
+            'description' => 'Your widgets', // 
+            'before_widget' => '<div id="%1$s" class="">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4>',
+            'after_title' => '</h4>'
+        )
+    ); */
+ 
+    /* Sidebar Main */
+    register_sidebar(
+        array(
+            'id' => 'sidebar_first',
+            'name' => 'Sidebar',
+            'description' => 'Your widgets.',
+            'before_widget' => '',
+            'after_widget' => '',
+            'before_title' => '<h4>',
+            'after_title' => '</h4>'
+        )
+    );
+}
+add_action( 'widgets_init', 'true_register_wp_sidebars' );
