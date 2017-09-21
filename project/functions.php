48b866e8568a311e71a05e02d4e0beb21d7fa4cf
Max <maxanjo512@gmail.com>
2017-09-21 15:25:43 +0400

    Update functions.php

--- project/functions.php
+++ project/functions.php
@@ -12,5 +12,8 @@ register_nav_menu('primary', 'Primary menu');
 // Load up our awesome theme options
 require  get_template_directory() . '/inc/function-admin.php';
 require  get_template_directory() . '/inc/enqueue.php';
-
+add_action('after_setup_theme', 'my_theme_setup');
+function my_theme_setup(){
+    load_theme_textdomain('webove', get_template_directory() . '/languages');
+}
 
