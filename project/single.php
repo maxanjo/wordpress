93a996ec9ede371d051cf57b3f48d4b46e6c63af
Max <maxanjo512@gmail.com>
2017-09-22 11:21:44 +0400

    Update single.php

--- project/single.php
+++ project/single.php
@@ -1,30 +1,31 @@
 <?php get_header(); ?>
 <!-- Content -->
-<div class="row single-page">
-    <div class="container">
-        <main class="col-sm-8">
-            <!-- Article -->
-            <?php if (have_posts()) : while (have_posts()) : the_post();?>
-                <article class="article">
-                  <div class="section-title text-center">
-                    <div class="h2" ><?php the_title(); ?></div>
-                </div>
-                <div class="media">
-                    <div class="media-img"><?php the_post_thumbnail(array(100, 100)); ?></div>
-                    <div class="media-body">
-                      <?php the_content(); ?>
-                  </div>
-                  <a class="backblog" href="<?php echo home_url(); ?>/blog"><?php _e('Back','webove'); ?></a> 
-              </div><!-- Media-->
-          </article>
-      <?php endwhile; else: ?>
-  <?php endif; ?>
-  <?php comments_template(); ?>
-        </main><!-- end Main -->
+<div class="blog-page">
+    <div class="row single-page">
+        <div class="container">
+            <main class="col-sm-8">
+                <!-- Article -->
+                <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                    <article class="article">
+                      <div class="section-title text-center">
+                         <h2 class="heading"><?php the_title(); ?></h2>
+                    </div>
+                    <div class="media">
+                        <div class="media-img"><?php the_post_thumbnail(array(100, 100)); ?></div>
+                        <div class="media-body">
+                          <?php the_content(); ?>
+                      </div>
+                      <a class="backblog button" href="<?php echo home_url(); ?>/blog"><?php _e('Back','webove'); ?></a> 
+                  </div><!-- Media-->
+              </article>
+          <?php endwhile; else: ?>
+      <?php endif; ?>
+      <?php comments_template(); ?>
+            </main><!-- end Main -->
 
-        <!-- Sidebar -->
-        <?php get_sidebar() ?>
-    </div><!-- end Container -->
-</div><!-- end Row -->
-
+            <!-- Sidebar -->
+            <?php get_sidebar() ?>
+        </div><!-- end Container -->
+    </div><!-- end Row -->
+</div>
 <?php get_footer(); ?>
