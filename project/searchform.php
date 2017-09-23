d5288614ad638353a17317bb4638701a3fc7a1f0
Max <maxanjo512@gmail.com>
2017-09-23 16:58:13 +0400

    Add files via upload

--- project/searchform.php
+++ project/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="searchform" class="wb_search" action="<?php echo home_url('/');?>">
-	<input type="text" class="form-control text" placeholder="<?php _e('Search','webove');?>" value="" name="s" id="s" title="search" />
-	<input type="submit" id="searchsubmit" class='btn' value="<?php _e('Search','webove');?>">
+	<input type="text" class="text" placeholder="<?php _e('Search','webove');?>" value="" name="s" id="s" title="search" />
+	<button  id="searchsubmit" class='button-search'><span class="icon-search"></span></button>
 </form>
