ce0a49b62ce95ff8ddcedbf83c98e863ed98fdf9
Max <maxanjo512@gmail.com>
2017-09-25 16:08:18 +0400

    Update search.php

--- project/search.php
+++ project/search.php
@@ -9,21 +9,7 @@ get_header(); ?>
                 <?php if ( have_posts() ) : 
           // Start the loop.
                 while ( have_posts() ) : the_post();?>
-                <article class="article">
-                    <div class="section-title text-center">
-                        <h2 class="heading"><?php the_title(); ?></h2>
-                    </div>
-                    <div class="media">
-                     <?php if ( has_post_thumbnail() ) :?>
-                    <div class="media-img"><?php the_post_thumbnail(array(360, 230)); ?>
-                      <?php endif;?>
-                        <div class="media-body">
-                            <?php the_content(); ?>
-                        </div>
-                        <a href="<?php the_permalink() ?>" class="permalink button"><?php _e('Learn more','webove'); ?></a>
-                    </div>
-                </article>
-
+                <?php get_template_part('template-parts/content') ?>
           <?php endwhile;?>
           <div class="pagination-wrapper">
             <div class="pagination">
