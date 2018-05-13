44b5c11bcdc678c3d133225ed0c28c26be98f940
Max <maxanjo512@gmail.com>
2018-05-13 22:36:53 +0400

    Update enqueue.php

--- project/inc/enqueue.php
+++ project/inc/enqueue.php
@@ -38,6 +38,12 @@ function web_load_admin_scripts($current_){
 			wp_enqueue_script('web-jquery');
 			wp_register_style('web-admin', get_template_directory_uri() . '/inc/css/template.admin.css', array(), '1.0.0', 'all');
 			wp_enqueue_style('web-admin');
+			wp_register_script('web-palete-color', get_template_directory_uri() . '/inc/js/libs/palette-color-picker/palette-color-picker.min.js', '1.0.0', true);
+			wp_enqueue_script('web-palete-color');
+			wp_register_style('web-palette-color', get_template_directory_uri() . '/inc/css/palette-color-picker.css', array(), '1.0.0', 'all');
+			wp_enqueue_style('web-palette-color');
+			wp_register_script('web-color-picker-settings', get_template_directory_uri() . '/inc/js/color-picker.js', '1.0.0', true);
+			wp_enqueue_script('web-color-picker-settings');
 		}
 		else{return;};
 	}
