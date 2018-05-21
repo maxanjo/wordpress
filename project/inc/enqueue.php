ab14b5821238a234601874c43fead54d4bfb7fb7
Max <maxanjo512@gmail.com>
2018-05-21 19:27:35 +0400

    Update enqueue.php

--- project/inc/enqueue.php
+++ project/inc/enqueue.php
@@ -7,6 +7,7 @@
 */
 
 function web_load_admin_scripts($current_){
+	$css = strpos($current_, 'my_theme_css');
 	wp_register_style('web-admin', get_template_directory_uri() . '/inc/css/template.admin.css', array(), '1.0.0', 'all');
 	wp_enqueue_style('web-admin');
 	wp_enqueue_media();
@@ -34,7 +35,9 @@ function web_load_admin_scripts($current_){
 		wp_register_script('web-editor', 'https://cdn.ckeditor.com/4.7.3/basic/ckeditor.js', '4.7.3', false);
 		wp_enqueue_script('web-editor');
 		wp_localize_script('web-jquery', 'directory', array( 'url' => get_template_directory_uri() ));
-	}else if('webove_page_my_theme_css' == $current_){
+	}else if(
+		$css !== false
+	){
 			wp_enqueue_script('ace', get_template_directory_uri() . '/inc/js/libs/ace/ace.js', array('jquery'), '1.2.8', true );
 			wp_enqueue_script('custom-css', get_template_directory_uri() . '/inc/js/custom-css.admin.js', array('jquery'), '1.0.0', true);
 			wp_register_script('web-jquery', get_template_directory_uri() . '/inc/js/jquery.min.js', '1.0.0', true);
@@ -48,10 +51,6 @@ function web_load_admin_scripts($current_){
 			   wp_dequeue_script( 'web-select-admin' );
 			}
 			add_action( 'wp_print_scripts', 'wpdocs_dequeue_script_select', 100 );
-		}elseif('webove_page_my_theme_Test' == $current_){
-		wp_register_script('web-editor', 'https://cdn.ckeditor.com/4.7.3/basic/ckeditor.js', '4.7.3', false);
-		wp_enqueue_script('web-editor');
-		wp_localize_script('web-jquery', 'directory', array( 'url' => get_template_directory_uri() ));
-	}else{return;};
+		}else{return;};
 	}
 add_action ('admin_enqueue_scripts', 'web_load_admin_scripts');
