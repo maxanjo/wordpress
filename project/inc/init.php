ab4646345d9b0b2c829b65df6854eaaf51e50ebf
Max <maxanjo512@gmail.com>
2018-05-21 19:30:32 +0400

    Update init.php

--- project/inc/init.php
+++ project/inc/init.php
@@ -6,5 +6,5 @@ $default_buttons_family = 'Abel, Abel';  //Buttons. With a comma!
 $default_buttons_size = '15';  //Buttons 
 $default_buttons_color = '#c70c0c';  //Buttons
 $default_background_color = '#fff';  //Background
-$default_background_position = 'left top, Left Top'; //Background position. With a comma! 
+$default_background_position = 'left top, Left Top'; //Background position: "option, Label". With a comma! 
 $default_background_repeat = 'repeat, Repeat'; //Background repeat
